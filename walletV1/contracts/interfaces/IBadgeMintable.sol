bd176bceb2d31086d7cb2c5da0daf986b579a0c9
ali-ahmed-05 <aliahmed.pluton@gmail.com>
2022-02-16 17:49:12 +0500

    asd
    asd

--- walletV1/contracts/interfaces/IBadgeMintable.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity >=0.8.0;
-
-
-interface IBadgeMintable {
-    function createToken(string memory _tokenURI , address account,uint8 _nftType) external returns(uint);
-    function addOwner(address owner_) external;
-}
